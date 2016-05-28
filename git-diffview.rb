e9ae7a61dd581ed8e9c2639214d11624ebb9d74f
Greg Werbin <ourobourbon@gmail.com>
2016-05-28 18:14:18 -0400

    Initial commit

---
+++ git-diffview.rb
@@ -0,0 +1,13 @@
+class GitDiffview < Formula
+  desc "A bash script to show the changes between two git paths as a treeview."
+  homepage "https://github.com/opicbernard/git-diffview"
+  url "https://github.com/opicbernard/git-diffview"
+  head "https://github.com/opicbernard/git-diffview"
+
+  depends_on "tree"
+
+  def install
+      bin.install "git-diffview"
+      (bin/"git-diffview").chmod 0755
+  end
+end
