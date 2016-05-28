d814cf40997d89c718a5b5e5ad111072299704be
Greg Werbin <ourobourbon@gmail.com>
2016-05-28 18:32:06 -0400

    Fix formula

--- git-diffview.rb
+++ git-diffview.rb
@@ -2,7 +2,7 @@ class GitDiffview < Formula
   desc "A bash script to show the changes between two git paths as a treeview."
   homepage "https://github.com/opicbernard/git-diffview"
   url "https://github.com/opicbernard/git-diffview"
-  head "https://github.com/opicbernard/git-diffview"
+  head "https://github.com/opicbernard/git-diffview.git"
 
   depends_on "tree"
 
