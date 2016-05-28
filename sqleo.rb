1380d7c532e3872892ba4333df11700105bcc60f
Greg Werbin <ourobourbon@gmail.com>
2016-05-28 18:47:27 -0400

    Add sqleo

---
+++ sqleo.rb
@@ -0,0 +1,13 @@
+class SQLeo < Formula
+    desc "A powerful SQL tool"
+    homepage "http://sqleo.sourceforge.net/"
+    version "2016.05.rc1"
+    url "http://downloads.sourceforge.net/project/sqleo/SQLeoVQB/SQLeoVQB.2016.04.rc1.zip?r=&ts=1464475389"
+    sha256 "8a396b3817b9291cd431cc7fc70be7c7356c0c29af30e60dcc431ae991e1840f"
+
+    bottle :unneeded
+
+    def install
+        bin.write_jar_script "SQLeoVQB.jar"
+    end
+end
