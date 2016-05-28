711231f7e1ea56641a8727bf5d6898d394b0b488
Greg Werbin <ourobourbon@gmail.com>
2016-05-28 19:28:02 -0400

    attempt fix

--- Casks/sqleovqb.rb
+++ Casks/sqleovqb.rb
@@ -1,10 +1,10 @@
-cask 'sqleo' do
+cask 'sqleovqb' do
     version "2016.05.rc1"
     sha256 "8a396b3817b9291cd431cc7fc70be7c7356c0c29af30e60dcc431ae991e1840f"
 
     url "http://downloads.sourceforge.net/project/sqleo/SQLeoVQB/SQLeoVQB.2016.04.rc1.zip?r=&ts=1464475389"
-    name 'SQLeo'
+    name 'SQLeoVQB'
     homepage "http://sqleo.sourceforge.net/"
 
-    app 'sqleo-2016.05.rc1/SQLeoVQB.jar'
+    app 'sqleovqb-2016.05.rc1/SQLeoVQB.jar'
 end
