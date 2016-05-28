bc5e8bb98896a32fe91c775429d624ee35d42e6c
Greg Werbin <ourobourbon@gmail.com>
2016-05-28 19:07:06 -0400

    attempt fix

--- sqleo.rb
+++ sqleo.rb
@@ -9,7 +9,7 @@ class Sqleo < Formula
 
     def install
         libexec.install "SQLeoVQB.jar"
-        (libexec/"sqleo-start.HELP").rename("sqleo-start.command"
+        (libexec/"sqleo-start.HELP").rename("sqleo-start.command")
         # libexec.write_jar_script libexec/"SQLeoVQB.jar", "SQLeoVQB.jar"
     end
 end
