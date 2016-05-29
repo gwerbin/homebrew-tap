5d965ff0e0806fa82c7653ec158ff9baf81b1eb5
Greg Werbin <ourobourbon@gmail.com>
2016-05-28 23:14:33 -0400

    Style compliance

--- Casks/sqleo.rb
+++ Casks/sqleo.rb
@@ -1,12 +1,11 @@
 cask 'sqleo' do
-    version "2016.05.rc1"
-    sha256 "8a396b3817b9291cd431cc7fc70be7c7356c0c29af30e60dcc431ae991e1840f"
+  version '2016.05.rc1'
+  sha256 '8a396b3817b9291cd431cc7fc70be7c7356c0c29af30e60dcc431ae991e1840f'
 
-    url "http://downloads.sourceforge.net/project/sqleo/SQLeoVQB/SQLeoVQB.2016.04.rc1.zip"
-    name "SQLeo Visual Query Builder"
-    homepage "http://sqleo.sourceforge.net/"
-    license :unknown
+  url 'http://downloads.sourceforge.net/project/sqleo/SQLeoVQB/SQLeoVQB.2016.04.rc1.zip'
+  name 'SQLeo Visual Query Builder'
+  homepage 'http://sqleo.sourceforge.net/'
+  license :unknown
 
-    app "SQLeoVQB.2016.04.rc1/SQLeoVQB.jar"
+  app 'SQLeoVQB.2016.04.rc1/SQLeoVQB.jar'
 end
-
