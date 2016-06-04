3d6d64fff167e2a6e70907ca449c882881471b15
Greg Werbin <ourobourbon@gmail.com>
2016-06-04 18:06:06 -0400

    Add neovim-qt formula

---
+++ Formula/neovim-qt.rb
@@ -0,0 +1,25 @@
+class NeovimQt
+  desc "Neovim client library and GUI, in Qt5."
+  homepage "https://github.com/equalsraf/neovim-qt"
+
+  head do
+    url "https://github.com/equalsraf/neovim-qt.git"
+
+    depends_on "cmake" => :build
+    depends_on "qt5"
+    depends_on "neovim"
+  end
+
+    def install
+      mkdir "build" do
+        ohai "Building Neovim-Qt"
+
+        # cmake_args = std_cmake_args + ["-DIconv_INCLUDE_DIRS:PATH=/usr/include",
+        #                                "-DIconv_LIBRARIES:PATH=/usr/lib/libiconv.dylib"]
+        # system "cmake", "..", *cmake_args
+ 
+        system "cmake", ".."
+        system "cmake", "--build", "."
+      end
+    end
+  end
