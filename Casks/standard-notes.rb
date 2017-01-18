4dad052a576bb07d55a3ccad5ca59c9337d4493f
greg <blackhole@flesh.space>
2017-01-18 02:30:40 -0500

    Fix a bug
    Apparently interpolation doesnt work after all

--- Casks/standard-notes.rb
+++ Casks/standard-notes.rb
@@ -1,7 +1,7 @@
 cask 'standard-notes' do
   version '0.1.1'
   sha256 '9b6a96ac9d00687f9fdd1ccd456ce36575228dd3a6b90662e8fe529bee04d955'
-  url 'https://github.com/standardnotes/desktop/releases/download/v#{version}/Standard.Notes-Mac-#{version}.dmg'
+  url 'https://github.com/standardnotes/desktop/releases/download/v0.1.1/Standard.Notes-Mac-0.1.1.dmg'
 
   name 'Standard Notes'
   homepage 'https://standardnotes.org/'
