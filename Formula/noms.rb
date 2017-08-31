bb9b3422e605936190e77e479fa5e6400875bec2
Greg Werbin <spam@me.gregwerbin.com>
2017-08-31 02:24:31 -0400

    Bump noms

--- Formula/noms.rb
+++ Formula/noms.rb
@@ -1,25 +1,21 @@
 class Noms < Formula
   desc "The versioned, forkable, syncable database"
   homepage "https://github.com/attic-labs/noms"
-  url "https://s3-us-west-2.amazonaws.com/download.noms.io/jobs/NomsBuildGoBinaries/83/noms-4ae6b16-darwin-amd64.tar.gz"
-  version "8.3"
-  sha256 "4c63ae5292136cd389abfaefe9008c5670aa2a482ca84ebc5e4b45038d74fa11"
+  head "https://s3-us-west-2.amazonaws.com/downloadstable.noms.io/jobs/NomsBuildGoBinaries-v7/4/noms-HEAD-darwin-amd64.tar.gz"
 
   def install
-    bin.install("nomdex")
-    bin.install("noms-merge")
-    bin.install("noms")
+    bin.install "nomdex"
+    bin.install "noms"
 
-    mv "blob-get", bin/"noms-blob-get"
-    mv "counter", bin/"noms-counter"
-    mv "csv-analyze", bin/"noms-csv-analyze"
-    mv "csv-export", bin/"noms-csv-export"
-    mv "csv-import", bin/"noms-csv-import"
-    mv "demo-server", bin/"noms-demo-server"
-    mv "json-import", bin/"noms-json-import"
-    mv "poke", bin/"noms-poke"
-    mv "url-fetch", bin/"noms-url-fetch"
-    mv "xml-import", bin/"noms-xml-import"
+    bin.install "blob-get"
+    bin.install "counter"
+    bin.install "poke"
+    bin.install "url-fetch"
+    bin.install "xml-import"
+
+    bin.install "csv-analyze"
+    bin.install "csv-export"
+    bin.install "json-import"
   end
 
   test do
