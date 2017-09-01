6c1ae2471ec0503b8b08b1a102355359a4ca1345
Greg Werbin <spam@me.gregwerbin.com>
2017-08-31 23:31:04 -0400

    Attempt download fix using Git strategy

--- Formula/sqlcheck.rb
+++ Formula/sqlcheck.rb
@@ -1,10 +1,18 @@
 class Sqlcheck < Formula
   desc "Automatically identify anti-patterns in SQL queries"
   homepage "https://github.com/jarulraj/sqlcheck"
-  url "https://github.com/jarulraj/sqlcheck/archive/1.1.tar.gz"
+  #url "https://github.com/jarulraj/sqlcheck/archive/1.1.tar.gz"
+  url "https://github.com/jarulraj/sqlcheck.git",
+    :using => :git,
+    :tag => "1.1",
+    :revision => "ac8a9a53be20bfd6f65bee6b74c077358b943ae6"
   sha256 "6c8d26c8ae79fe49c785ef0bd32210ba3019a9805dd3debb568bd5a95ecb3201"
 
   depends_on "cmake" => :build
+
+  head do
+    url "https://github.com/jarulraj/sqlcheck.git"
+  end
 
   def install
     system "./bootstrap"
