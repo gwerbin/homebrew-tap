fe933d8d0e37177c32543871ed4c900cdaebff6a
Greg Werbin <spam@me.gregwerbin.com>
2017-10-12 17:58:51 -0400

    Add GitBook editor

---
+++ Casks/gitbook-editor.rb
@@ -0,0 +1,10 @@
+cask 'gitbook-editor' do
+  version '7.0.12'
+  sha256 '7dfec6baf5c509f5b48e5c85ca09e764531de3ba42e4f068b9a85e07881de05c'
+
+  url 'https://www.gitbook.com/editor/osx/download'
+  name 'GitBook Editor'
+  homepage 'https://gitbook.com/editor'
+
+  app 'GitBook Editor.app'
+end
