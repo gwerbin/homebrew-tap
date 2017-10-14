52356b9af3d26830896b332ce02c5c9e405a0dfb
Greg Werbin <spam@me.gregwerbin.com>
2017-10-13 22:06:56 -0400

    Add test block for git-diffview

--- Formula/git-diffview.rb
+++ Formula/git-diffview.rb
@@ -1,5 +1,5 @@
 class GitDiffview < Formula
-  desc "A bash script to show the changes between two git paths as a treeview."
+  desc "Show the changes between two git paths as a treeview."
   homepage "https://github.com/opicbernard/git-diffview"
   head "https://github.com/opicbernard/git-diffview.git"
 
@@ -8,5 +8,26 @@ class GitDiffview < Formula
   def install
     bin.install "git-diffview"
     (bin/"git-diffview").chmod 0755
+  end
+
+  test do
+    mkdir "repo"
+    cd "repo"
+    system "git init"
+    # need at least 2 commits for it to work, since
+    # the script assumes the presence of a HEAD~ revision.
+    system "touch", "foo"
+    system "git", "add", "foo"
+    system "git", "commit", "-m", "Add 'foo'"
+    system "touch", "bar"
+    system "git", "add", "bar"
+    system "git", "commit", "-m", "Add 'bar'"
+    system "touch", "baz"
+    system "git", "add", "baz"
+    system "git", "commit", "-m", "Add 'baz'"
+    stdout, stderr, status = Open3.capture3 "git-diffview"
+    # stdout == "bar\ A\nbaz\ A"
+    # stderr == ""
+    # status == 0 
   end
 end
