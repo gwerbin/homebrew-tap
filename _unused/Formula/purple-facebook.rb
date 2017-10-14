143654c7ae9273f75a55a0f96ca8b5959007620d
Greg Werbin <spam@me.gregwerbin.com>
2017-10-14 01:53:34 -0400

    Attempt to get it working

--- _unused/Formula/purple-facebook.rb
+++ _unused/Formula/purple-facebook.rb
@@ -2,8 +2,9 @@ class PurpleFacebook < Formula
   desc "Facebook protocol plugin for libpurple"
   homepage "https://github.com/dequis/purple-facebook"
   url "https://github.com/dequis/purple-facebook/releases/download/v0.9.5-9ff9acf9fa14/purple-facebook-0.9.5-9ff9acf9fa14.tar.gz"
-  version "0.9.5-9ff9acf9fa14"
   sha256 "7ab652dd0430166465f820e6e72bf6fffe09db936b535c212e571ec1742146dc"
+  version "0.9.5-9ff9acf9fa14"
+  version_scheme 1
 
   depends_on "pidgin"
   depends_on "glib" => :build
@@ -12,7 +13,8 @@ class PurpleFacebook < Formula
   def install
     ohai "testing"
     system "./configure", "--disable-dependency-tracking",
-                          "--disable-silent-rules"
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}"
     system "make"
     system "make", "install"
   end
