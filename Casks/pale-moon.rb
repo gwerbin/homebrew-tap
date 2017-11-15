a5953405c25eab842ee7790aa0282b77f60702de
Gregory Werbin <spam@me.gregwerbin.com>
2017-11-15 01:04:19 -0500

    Upgrade Pale Moon version

--- Casks/pale-moon.rb
+++ Casks/pale-moon.rb
@@ -1,8 +1,8 @@
 cask 'pale-moon' do
-  version '27.5.0'
-  sha256
+  version '27.6.0'
+  sha256 'bbc66f26c422667ce2d33dbf6bfc07d4c4583830a7a1dac5eeef6cb804bd0e9f'
 
-  url 'http://mac.palemoon.org/dist/palemoon-27.5.0.mac64.dmg'
+  url 'http://mac.palemoon.org/dist/palemoon-27.6.0.mac64.dmg'
   name 'Pale Moon'
   homepage 'https://palemoon.org'
 
