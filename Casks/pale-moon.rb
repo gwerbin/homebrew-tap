28f98b60fd2f37f8562df6c7268430f08bf7cdfa
Gregory Werbin <spam@me.gregwerbin.com>
2017-11-15 00:46:43 -0500

    Add Pale Moon

---
+++ Casks/pale-moon.rb
@@ -0,0 +1,10 @@
+cask 'pale-moon' do
+  version '27.5.0'
+  sha256
+
+  url 'http://mac.palemoon.org/dist/palemoon-27.5.0.mac64.dmg'
+  name 'Pale Moon'
+  homepage 'https://palemoon.org'
+
+  app 'Palemoon.app'
+end
