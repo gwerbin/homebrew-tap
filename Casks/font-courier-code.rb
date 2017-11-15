c5dd4d612ff7bbf18ddc6eeb074bad7a5649725a
Gregory Werbin <spam@me.gregwerbin.com>
2017-11-15 00:47:18 -0500

    Add (provisionally) Courier Code font

---
+++ Casks/font-courier-code.rb
@@ -0,0 +1,13 @@
+cask 'font-courier-code' do
+  version '2.0'
+  sha256 '43dde60cae5f8801d21924fcc17af4665b7e06735e113158d59a5dbd48402920'
+
+  url 'https://fontlibrary.org/assets/downloads/courier-code/d0b044fc63eb2e97d49d433acac9053d/courier-code.zip'
+  name 'Courier Code'
+  homepage 'https://fontlibrary.org/en/font/courier-code'
+
+  font 'CourierCode-Bold.ttf'
+  font 'CourierCode-BoldItalic.ttf'
+  font 'CourierCode-Italic.ttf'
+  font 'CourierCode-Roman.ttf'
+end
